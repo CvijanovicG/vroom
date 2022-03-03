d3ac7b155f23c1f8ce0637becdf800ee3dea62d7
CvijanovicG <cvijanovicg@gmail.com>
2022-03-03 09:26:02 +0100

    test

---
+++ vroom/Controllers/MakeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using vroom.Models;
+
+namespace vroom.Controllers
+{
+    public class MakeController : Controller
+    {
+        //make/bikes
+        public IActionResult Bikes()
+        {
+            Make make = new Make { Id=1, Name = "Harlay Davidson" };
+            return View(make);
+        }
+    }
+}
--- vroom/Models/Model.cs
+++ vroom/Models/Model.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,6 +11,8 @@ namespace vroom.Models
 
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
         public Make Make { get; set; }
         [ForeignKey("Make")]
