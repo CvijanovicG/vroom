59b72729b8f2f26086d4bfa6fa913c3e822be28e
CvijanovicG <cvijanovicg@gmail.com>
2022-03-04 14:16:33 +0100

    Branch_7

--- vroom/Controllers/ModelController.cs
+++ vroom/Controllers/ModelController.cs
@@ -36,7 +36,7 @@ namespace vroom.Controllers
         {
             return View(ModelVM);
         }
-        [HttpPost,ActionName("Create")]
+        [HttpPost, ActionName("Create")]
         public IActionResult CreatePost()
         {
             if (!ModelState.IsValid)
@@ -47,5 +47,42 @@ namespace vroom.Controllers
             _db.SaveChanges();
             return RedirectToAction(nameof(Index));
         }
+
+        public IActionResult Edit(int id)
+        {
+            ModelVM.Model = _db.Models.Include(m => m.Make).SingleOrDefault(m => m.Id == id);
+            if (ModelVM.Model == null)
+            {
+                return NotFound();
+            }
+            return View(ModelVM);
+
+        }
+        [HttpPost, ActionName("Edit")]
+        public IActionResult EditPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(ModelVM);
+            }
+            _db.Update(ModelVM.Model);
+            _db.SaveChanges();
+            return RedirectToAction(nameof(Index));
+
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            Model model = _db.Models.Find(id);
+            if (model == null)
+            {
+                return NotFound();
+            }
+            _db.Models.Remove(model);
+            _db.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
     }
 }
