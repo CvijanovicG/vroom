fbb42772580d05014a2b631ca92a9bd0494ba678
CvijanovicG <cvijanovicg@gmail.com>
2022-03-08 12:40:36 +0100

    Branch_7

---
+++ vroom/Extensions/ReflectionExtensioins.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace vroom.Extensions
+{
+    public static class ReflectionExtensioins
+    {
+        public static string GetPropetryValue<T>(this T Item, string propertyName)
+        {
+            return Item.GetType().GetProperty(propertyName).GetValue(Item, null).ToString();
+        }
+
+    }
+}
