6f4885edec6355efa0615d5c7d89574c6544a7bc
CvijanovicG <cvijanovicg@gmail.com>
2022-03-11 15:11:03 +0100

    Branch_10

--- vroom/Controllers/MakeController.cs
+++ vroom/Controllers/MakeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,8 +9,10 @@ using vroom.Models;
 
 namespace vroom.Controllers
 {
+    [Authorize(Roles ="Admin,Executive")]
     public class MakeController : Controller
     {
+        
         private readonly VroomDbContext _db;
 
         public MakeController(VroomDbContext db)
--- vroom/Controllers/ModelController.cs
+++ vroom/Controllers/ModelController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -10,6 +11,7 @@ using vroom.Models.ViewModel;
 
 namespace vroom.Controllers
 {
+    [Authorize(Roles = "Admin,Executive")]
     public class ModelController : Controller
     {
         private readonly VroomDbContext _db;
